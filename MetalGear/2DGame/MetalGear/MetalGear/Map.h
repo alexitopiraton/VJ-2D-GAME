516c0be3d286216615694387058e238e2367d94b
alexitopiraton <02alex448@gmail.com>
2025-09-18 22:08:03 +0200

    Class Map created. Outside screens design done

--- MetalGear/2DGame/MetalGear/MetalGear/Map.h
+++ MetalGear/2DGame/MetalGear/MetalGear/Map.h
@@ -1,7 +1,28 @@
 #ifndef _MAP_INCLUDE
 #define _MAP_INCLUDE
 
+#include <glm/glm.hpp>
+#include "Texture.h"
+#include "ShaderProgram.h"
+
 class Map {
+private:
+	Map();
+
+public:
+	static Map *createMap();
+	void render(const Texture& tex) const;
+
+	bool collisionMoveLeft();
+	bool collisionMoveRight();
+	bool collisionMoveDown();
+	bool collisionMoveUp();
+
+private:
+	GLuint vao;
+	GLuint vbo;
+	GLint posLocation, texCoordLocation;
+
 
 };
 
